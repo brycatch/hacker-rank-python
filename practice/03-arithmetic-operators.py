453d4f0b181466d5cd1f71415887b8bbfc6fbd07
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2021-01-30 13:12:12 -0600

    03 arithmetic operators

---
+++ practice/03-arithmetic-operators.py
@@ -0,0 +1,22 @@
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+
+
+def get_sum(a, b):
+    print(a + b)
+
+
+def get_difference(a, b):
+    print(a - b)
+
+
+def get_product(a, b):
+    print(a * b)
+
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+
+    get_sum(a, b)
+    get_difference(a, b)
+    get_product(a, b)
